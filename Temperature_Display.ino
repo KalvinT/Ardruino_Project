cba88795cda0808c867b8301bee6c0851cd8069e
KalvinT <81987519+KalvinT@users.noreply.github.com>
2021-04-05 13:43:35 -0500

    Update Temperature_Display.ino

--- Temperature_Display.ino
+++ Temperature_Display.ino
@@ -1,5 +1,7 @@
 #include <OneWire.h> 
 #include <DallasTemperature.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 /********************************************************************/
 // Data wire is plugged into pin 2 on the Arduino 
 #define ONE_WIRE_BUS 2 
@@ -7,14 +9,21 @@
 // Setup a oneWire instance to communicate with any OneWire devices  
 // (not just Maxim/Dallas temperature ICs) 
 OneWire oneWire(ONE_WIRE_BUS); 
+LiquidCrystal_I2C lcd (0x27,16,2); // set the LCD address to 0x27 for a16 chars and 2 line display
+
 /********************************************************************/
 // Pass our oneWire reference to Dallas Temperature. 
 DallasTemperature sensors(&oneWire);
 /********************************************************************/ 
 void setup(void) 
 { 
- // start serial port 
- Serial.begin(9600); 
+   lcd.begin();
+
+ // Turn on the blacklight and print a message.
+  lcd.backlight();
+  lcd.print("Hi");
+  Serial.begin(9600);
+ // start serial port  
  Serial.println("Dallas Temperature IC Control Library Demo"); 
  // Start up the library 
  sensors.begin(); 
@@ -32,5 +41,10 @@ void loop(void)
  Serial.print(sensors.getTempCByIndex(0)); // Why "byIndex"?  
    // You can have more than one DS18B20 on the same bus.  
    // 0 refers to the first IC on the wire 
+   lcd.clear();
+   lcd.print("Tcurr = ");
+   lcd.print(sensors.getTempCByIndex(0));
+   lcd.print(" C");
    delay(1000); 
 } 
+
