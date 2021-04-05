6135087bc2c168387ad881aae15b3fc754ee3989
KalvinT <81987519+KalvinT@users.noreply.github.com>
2021-04-05 13:28:07 -0500

    Update Temperature_Display.ino

--- Temperature_Display.ino
+++ Temperature_Display.ino
@@ -1,19 +1,36 @@
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-
-
-LiquidCrystal_I2C lcd (0x27,16,2); // set the LCD address to 0x27 for a16 chars and 2 line display
-
-void setup() {
- lcd.begin();
-
- // Turn on the blacklight and print a message.
-  lcd.backlight();
-  lcd.print("Hello, World");
-  Serial.begin(9600);
-}
-
-// the loop function runs over and over again forever
-void loop() {
-//
-}
+#include <OneWire.h> 
+#include <DallasTemperature.h>
+/********************************************************************/
+// Data wire is plugged into pin 2 on the Arduino 
+#define ONE_WIRE_BUS 2 
+/********************************************************************/
+// Setup a oneWire instance to communicate with any OneWire devices  
+// (not just Maxim/Dallas temperature ICs) 
+OneWire oneWire(ONE_WIRE_BUS); 
+/********************************************************************/
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+/********************************************************************/ 
+void setup(void) 
+{ 
+ // start serial port 
+ Serial.begin(9600); 
+ Serial.println("Dallas Temperature IC Control Library Demo"); 
+ // Start up the library 
+ sensors.begin(); 
+} 
+void loop(void) 
+{ 
+ // call sensors.requestTemperatures() to issue a global temperature 
+ // request to all devices on the bus 
+/********************************************************************/
+ Serial.print(" Requesting temperatures..."); 
+ sensors.requestTemperatures(); // Send the command to get temperature readings 
+ Serial.println("DONE"); 
+/********************************************************************/
+ Serial.print("Temperature is: "); 
+ Serial.print(sensors.getTempCByIndex(0)); // Why "byIndex"?  
+   // You can have more than one DS18B20 on the same bus.  
+   // 0 refers to the first IC on the wire 
+   delay(1000); 
+} 
